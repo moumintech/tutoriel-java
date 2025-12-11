1c634f6aae0def9bcfcab82c2c00af5810f181ff
Zam <148615828+moumintech@users.noreply.github.com>
2025-12-11 18:23:46 +0100

    Create Film class with main method
    Fix syntax errors and update film title.

---
+++ Concepts-base/Film.java
@@ -0,0 +1,10 @@
+class Film {
+  public static void main(String[] args]{
+     String titre = "Matrix";
+    titre = "Cendrillon";
+System.out.println("Mon fim préféré est"titre)
+  }
+}
+
+
+                    
