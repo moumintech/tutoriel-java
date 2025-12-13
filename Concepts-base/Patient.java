2d44a1861f4b417f08f0d85bbfa918121ed966ec
Zam <148615828+moumintech@users.noreply.github.com>
2025-12-13 08:01:27 +0100

    exercice fait

---
+++ Concepts-base/Patient.java
@@ -0,0 +1,13 @@
+public class Patient{
+   Public static void main(String[] args) {
+      String nom = "Paul";
+      int  age = 20;
+      double  temperature = 36.5;
+      boolean hospitalise = true;
+
+  System.out.println("Nom du Patient :" + nom);
+  System.out.println("Age:" + age);
+  System.out.println("Température : "+ temperature);
+  System.out.println("Hospitaliser :" hospitalise);
+   }
+}
