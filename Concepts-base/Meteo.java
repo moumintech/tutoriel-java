66d6fc9f403202f51866ba62363f6747a7ad88b0
Zamzam <148615828+moumintech@users.noreply.github.com>
2025-12-26 22:56:01 +0100

    Update Meteo.java

--- Concepts-base/Meteo.java
+++ Concepts-base/Meteo.java
@@ -1,8 +1,13 @@
 public class Meteo {
-    public static void main(String[] args) {
-        System.out.println("Ville : Mayotte");
-        System.out.println("Température : 36°");
-    }
+
+	public static void main(String[] args) {
+		
+ System.out.println("Le nom de la ville: Paris");
+ System.out.println("La température : 3");
+ System.out.println("Le type de météo : Nuageux");
+
+	}
+
 }
 
 //Cette exercice est corrigé.
