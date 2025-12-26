c81dff670ad1ea9cba1cfc02f44e765d97b3e8df
Zamzam <148615828+moumintech@users.noreply.github.com>
2025-12-27 00:04:13 +0100

    Update and rename Meteo.java to CommandeEnLigne.java

---
+++ Concepts-base/CommandeEnLigne.java
@@ -0,0 +1,23 @@
+public class CommandeEnLigne {
+
+	public static void main(String[] args) {
+		
+double prixProduit = 15 ;
+int quantiteCommande= 6;
+double soldeClient = 300;
+boolean statutClientPremium = true ;
+ double prixTotal = prixProduit * quantiteCommande;
+ 
+ if ((soldeClient >= prixTotal)&& quantiteCommande > 0 || statutClientPremium ) {
+	 System.out.println("Achat accepté");
+ } else {
+	 System.out.println("Achat refusé");
+	 
+ }
+ System.out.println("Le prix total " + prixTotal );
+ System.out.println("solde du client " + soldeClient );
+	}
+
+}
+
+//Cette exercice est corrigé.
